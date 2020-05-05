d7424563752778f7086ce2bb4f87fb7ff8c37235
16131 <zakitylox@gmail.com>
2020-05-05 05:27:56 +0200

    Done : all working (even empty form)
    need style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './index.less';
+import { withRouter } from 'react-router-dom';
 
-export default () => {
-  return (
-    <div
-      style={{
-        position: 'relative',
-        width: '600px',
-        height: '500px ',
-      }}
-    >
-      <h1 className={styles.title}>Shayp UmiJs</h1>
-      <form>
-        <label>
-          Name :
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ' ',
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    // alert("Thank you " + this.state.name);
+    let pathing = '/hello/' + this.state.name;
+    this.props.history.push(pathing);
+    event.preventDefault();
+  }
+  handleChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  render() {
+    return (
       <div
         style={{
-          position: 'absolute',
-          bottom: '5px',
+          position: 'relative',
+          width: '600px',
+          height: '500px ',
         }}
       >
-        <h3>
-          <a href="about"> About </a>
-        </h3>
+        <h1 className={styles.title}>Shayp UmiJs</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name :
+            <input type="text" name="name" onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <div
+          style={{
+            position: 'absolute',
+            bottom: '5px',
+          }}
+        >
+          <h3>
+            <a href="about"> About </a>
+          </h3>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+export default withRouter(NameForm);
